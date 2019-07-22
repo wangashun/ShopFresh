a2ebe136656ca7b9aa6b461b2cf533f18705fc79
wangyaozeng <1040747384@qq.com>
2019-07-22 12:22:22 +0800

    项目建库及静态文件创建

--- ShopProject/ShopProject/settings.py
+++ ShopProject/ShopProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ShopProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
